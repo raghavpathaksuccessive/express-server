43dd65b67e5b441e876a32789795418e835dc465
raghavpathaknodejs <gpathak79@gmail.com>
2022-03-01 23:22:50 +0530

    feature day 5 day 6 day 7 day 8

---
+++ src/config/IConfig.ts
@@ -0,0 +1,8 @@
+interface Iconfig {
+    PORT: number;
+    NODE_ENV: string;
+    SECRET_KEY: string;
+    password: string;
+
+    }
+    export { Iconfig };
---
+++ src/server.ts
@@ -0,0 +1,45 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { notFoundHandler, errorHandler } from './libs/routes';
+
+import * as cors from 'cors';
+
+class Server {
+    app;
+    constructor(private config) {
+         this.app = express();
+    }
+    bootstrap() {
+        this.initBodyParser();
+        this.setupRoutes();
+            return this;
+    }
+    public setupRoutes() {
+         const { app } = this;
+         app.use(cors());
+        app.get('/health-check', ( req, res, next) => {
+              res.send('I am Ok');
+        });
+       
+      
+        app.use(notFoundHandler);
+        app.use(errorHandler);
+          return this;
+    }
+    public initBodyParser() {
+        // this.app.use(bodyParser.json());
+    }
+    run() {
+        const { PORT, NODE_ENV , MONGO_URL } = this.config;
+
+                this.app.listen(PORT, () => {
+                    const message = `|| App is running at port '${PORT}' in '${NODE_ENV}' mode ||`;
+                    console.log(message);
+                });
+   
+        return this;
+     
+    }
+
+}
+export default Server;
